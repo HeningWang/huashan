1e7db9906ebda1b68b6a72b65a5c7320f909d7d8
fabianSchlotterbeck <fabian.schlotterbeck@gmail.com>
2022-03-16 13:47:56 +0100

    adjust output file names

--- save_data.php
+++ save_data.php
@@ -1,6 +1,6 @@
  <?php
 $txt = $_POST["json"];
-$filename="data/".uniqid("pfaender").".json";
+$filename="data/".uniqid("huashan").".json";
 file_put_contents($filename, $txt);
 chmod($filename, 0640)
 ?> 
