45bf27b6df0e96392a8d7b3123bb780ee9bc7a31
fabianSchlotterbeck <58473746+fabianSchlotterbeck@users.noreply.github.com>
2022-03-29 16:27:06 +0200

    conduct initial analysis

--- analysis/preprocessing_and_decriptive_stats.R
+++ analysis/preprocessing_and_decriptive_stats.R
@@ -26,7 +26,8 @@ subj_info <-read.csv(file = "../data/huashan_subj_info.csv")
 xtabs(~id, data=data)
 xtabs(~id+conditions, data=data)#TODO: check
 xtabs(~id+leftright_trial, data=data)#TODO: check
-xtabs(~id+list, data=data)#TODO: check
+xtabs(~id+list, data=data)
+round(xtabs(~list, data=data)/180)
 
 
 xtabs(~conditions, data=data[data$item==1,])
@@ -60,49 +61,59 @@ data <- subset(data, !is.na(data$slider_value))
 #TODO: adjust later, rename variable
 data$prefer_first_1st <- ifelse(data$leftright_trial=="1left", 100-data$slider_value, data$slider_value)
 
-aggregate(data$prefer_subj_1st, list(data$conditions), FUN = function(x){c(mean(x), sd(x)/sqrt(length(x)))})
-aggregate(data$prefer_subj_1st, list(data$conditions, data$dist), FUN = function(x){c(mean(x), sd(x)/sqrt(length(x)))})
+aggregate(data$prefer_first_1st, list(data$conditions), FUN = function(x){c(mean(x), sd(x)/sqrt(length(x)))})
+aggregate(data$prefer_first_1st, list(data$conditions, data$dist), FUN = function(x){c(mean(x), sd(x)/sqrt(length(x)))})
 
-# #TODO: adjust later
-# #data_filler <- subset(data, conditions %in% c("filler_false", "filler_true"))
+
+#TODO: adjust later
+
+
+data_filler <- subset(data, str_sub(data$conditions, 1, 1) == "f")
+
+data <- droplevels(subset(data, str_sub(data$conditions, 1, 1) != "f"))
+
 # 
-# data <- subset(data, conditions %in% c("farbe_scharf", "farbe_unscharf", "grosse_scharf", "grosse_unscharf"))
-# 
-# 
-# 
+# #exclude pps
 # exclude<-NULL
 # #TODO: adjust
 # #exclude <- c()
 # 
 # data <- subset(data, !(id %in% exclude))
-#           
-# aggregate(data$prefer_subj_1st, list(data$conditions), 
-#           FUN=function(x){c(mean(x, na.rm = TRUE),sd(x, na.rm = TRUE)/sqrt(length(x)))})
-# 
-# 
-# ratings_aggregated<-aggregate(prefer_subj_1st~conditions, data=data, mean)
-# #TODO: find another way to compute ses (SEwithin?)
-# ses_ratings<-aggregate(prefer_subj_1st~conditions, data=data, function(x) {sd(x)/sqrt(length(x))})
-# ratings_aggregated<-cbind(ratings_aggregated,ses_ratings[,2])
-# 
-# names(ratings_aggregated)<-c("condition", "prefer_subj_1st", "se")
-# 
-# 
-# p <- ggplot(ratings_aggregated, aes(x=condition, y=prefer_subj_1st, fill=condition)) + 
-#   geom_bar(stat="identity", position=position_dodge()) +
-#   geom_errorbar(aes(ymin=prefer_subj_1st-se, ymax=prefer_subj_1st+se), width=.2,
-#                 position=position_dodge(.9))
-# 
-# #p + scale_fill_brewer(palette="Paired") + theme_minimal()            
-# p
-# pdf(file = file.path(plots_dir,"mean_preference.pdf"))
-# p
-# dev.off()
+
+data$combination <- as.factor(ifelse(str_sub(data$conditions, 3, 4)=="cf", "color_form",
+                           ifelse(str_sub(data$conditions, 3, 4)=="dc", "dimension_color",
+                                  "dimension_form")))
+
+
+data$relevant_property <- as.factor(ifelse(str_sub(data$conditions, 1, 1)== "e", "first",
+                                           ifelse(str_sub(data$conditions, 1, 1)== "z", "second",
+                                           "both")))
+                                           
+
+
+ratings_aggregated<-aggregate(prefer_first_1st~combination+relevant_property+dist, data=data, mean)
+#TODO: find another way to compute ses (SEwithin?)
+ses_ratings<-aggregate(prefer_first_1st~combination+relevant_property+dist, data=data, function(x) {sd(x)/sqrt(length(x))})
+ratings_aggregated<-cbind(ratings_aggregated,ses_ratings[,4])
+
+names(ratings_aggregated)[5]<-"se"
+
+
+p <- ggplot(ratings_aggregated, aes(x=combination, y=prefer_first_1st, fill=relevant_property)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=prefer_first_1st-se, ymax=prefer_first_1st+se), width=.2,
+                position=position_dodge(.9))+
+  facet_wrap(~dist)+
+  scale_fill_brewer(palette="Paired") + theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust=1))
+p
+pdf(file = file.path(plots_dir,"mean_preference_facet.pdf"))
+p
+dev.off()
 # 
 # 
 # data$relevant_prop <- ifelse(str_detect(data$conditions,"farbe"), "color", "size")
 # 
-# data$sharpness <- ifelse(str_detect(data$conditions,"un"), "low", "high")
 # 
 # m0 <- lmer(prefer_subj_1st~relevant_prop*sharpness+(1|id), data=data)
 # summary(m0)
